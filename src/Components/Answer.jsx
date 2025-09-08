cbf9e7ca6ec65d0cabe394bd04b2bc0b524b3f74
Shahzaib Ali <[shahxaibali34@gmail.com]>
2025-09-09 01:15:44 +0500

    Initial commit

---
+++ src/Components/Answer.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import { checkHeading, replaceHeadingStars } from '../helper'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import ReactMarkdown from 'react-markdown'
+
+export const Answer = ({ ans, totalResult, index, type }) => {
+  const [heading, setHeading] = useState(false)
+  const [answer, setAnswer] = useState(ans)
+
+  useEffect(() => {
+    if (checkHeading(ans)) {
+      setHeading(true)
+      setAnswer(replaceHeadingStars(ans))
+    }
+  }, [ans]) // Add ans as dependency
+
+  const components = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || '')
+      return !inline && match ? (
+        <SyntaxHighlighter
+          {...props}
+          children={String(children).replace(/\n$/, '')}
+          language={match[1]}
+          style={dark}
+          PreTag="div"
+        />
+      ) : (
+        <code {...props} className={className}>
+          {children}
+        </code>
+      )
+    }
+  }
+
+  return (
+    <>
+      {index === 0 && totalResult > 1 ? (
+        <span className='pt-2 text-xl block text-white'>{answer}</span>
+      ) : heading ? (
+        <span className='pt-2 text-lg block text-white'>{answer}</span>
+      ) : (
+        <span className={type === 'q' ? 'pl-1' : 'pl-5'}>
+          <ReactMarkdown components={components}>{answer}</ReactMarkdown>
+        </span>
+      )}
+    </>
+  )
+}
